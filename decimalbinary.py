cdede80a298af2ee3ee175b491879499964ad1c3
Omkar960 <wannaagg@gmail.com>
2025-08-26 13:27:25 +0100

    initial commit

---
+++ decimalbinary.py
@@ -0,0 +1,13 @@
+decimal = int(input("Enter a decimal number"))
+binary = 0
+
+binary = ""
+while decimal > 0:
+    remainder = decimal % 2
+    binary = str(remainder) + binary
+    decimal = decimal // 2
+print(binary)
+
+
+
+
