3cd8b2b49b877feaf07642bb7275e580a9a04ab8
Omkar960 <wannaagg@gmail.com>
2025-08-26 11:34:44 +0100

    initial commit

---
+++ primenumberss.py
@@ -0,0 +1,12 @@
+lowernumber = int(input("Enter a low number: "))
+highnumber = int(input("Enter a high number: "))
+
+print("Prime numbers between",lowernumber,"and",highnumber,"are: ")
+
+for num in range(lowernumber,highnumber + 1):
+    if num > 1:
+        for i in range (2,num):
+            if(num%i) == 0:
+                break
+            else:
+             print(num)
