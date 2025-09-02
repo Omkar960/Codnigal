41e1f1f8fb9ea19c5d99dd66d3aa5032e08906cd
Omkar960 <wannaagg@gmail.com>
2025-09-02 11:42:14 +0100

    initial commit

---
+++ infinite..py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().bgcolor("aqua")
+turtle.Screen().title("Turtle")
+pen = turtle.Turtle()
+size = 0
+
+while True:
+    for i in range(4):
+        pen.forward(size+1)
+        pen.left(90)
+        size = size - 5
+    size = size + 1
