81033f0d39a33f036a2243930f3f7734236361d5
Omkar960 <wannaagg@gmail.com>
2025-09-30 17:38:07 +0100

    inital commit

---
+++ mths.py
@@ -0,0 +1,18 @@
+import math
+
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+print(math.ceil(45.4))
+print(math.floor(45.8))
+print(math.factorial(5))
+print(math.sqrt(16))
+print(math.copysign(45,-27))
+print(math.exp(3))
+print(math.radians(30))
+print(math.gcd(12,56,60))
+print(math.fabs(-30.7))
+
+
+
+
