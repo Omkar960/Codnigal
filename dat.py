970d17a7a5276d874c4a41cf06bfad1ad628ece3
Omkar960 <wannaagg@gmail.com>
2025-10-07 17:14:00 +0100

    initial commit

---
+++ dat.py
@@ -0,0 +1,10 @@
+from datetime import date, time, datetime
+
+today = date.today()
+now = datetime.now()
+
+print("Today's date is," ,today )
+print("The current time and date is", now)
+
+print(today.year,today.day,today.month)
+
