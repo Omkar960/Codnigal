b722cde0c9240e527f67f3ba291e0c557484c9a3
Omkar960 <wannaagg@gmail.com>
2025-10-14 17:18:54 +0100

    initial commit

---
+++ tup.py
@@ -0,0 +1,16 @@
+tuple = (6,"Hi",True,6.7)
+print(tuple)
+
+tuplex = (1,2,3,4,5,6,7,8,9,)
+print(tuplex)
+tuplex = tuplex + (9,)
+print("tuplex")
+print(tuplex.count(5))
+
+tuples = (2,4,6,8,10)
+slice = tuples[3:5]
+print(slice)
+slices = tuples[:6]
+print(slices)
+slicex = tuples[1:]
+print(slicex)
