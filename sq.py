aac51d68f18a04b38cbfa1ccb61643883bd66f17
Omkar960 <wannaagg@gmail.com>
2025-10-14 17:56:40 +0100

    initial commit

--- sq.py
+++ sq.py
@@ -1,8 +1,18 @@
 start = int(input("Enter start value: "))
 end = int(input("Enter end value: "))
 
+number = 0
+for i in range( start, end + 1):
+    
+    print(i*i)
+    if i*i % 2 == 0:
+        print("It's even")
+    else:
+        print("It's odd")
+  
 
-square_list = [i**2 for i in range(start, end + 1)]
 
 
-print(square_list)
+
+
+
