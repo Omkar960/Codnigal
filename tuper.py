28b279d3de64df5a3a4dbedb48ece401ad7df0e2
Omkar960 <wannaagg@gmail.com>
2025-10-16 18:32:12 +0100

    initial commit

---
+++ tuper.py
@@ -0,0 +1,12 @@
+tuple1 = (4,3,2,2,-1,18)
+tuple2 = (2,4,8,8,3,2,9)
+number = 1
+number1 =1 
+a = 0
+for i in tuple1:
+    number *= i
+print("Product for tup1 is", number)
+
+for t in tuple2:
+    number1 *= t
+print("Product for tup2 is",number1)
