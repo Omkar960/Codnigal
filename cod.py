5b79a65844412e177c0fbef2dd5533f6f2dc322e
Omkar960 <wannaagg@gmail.com>
2025-10-17 17:11:32 +0100

    initial commit

---
+++ cod.py
@@ -0,0 +1,6 @@
+dic1 = {"India":"0091","Austria":"5456","Nepal":"6767"}
+
+print("Country code for India")
+print(dic1.get('India',"Country no found -"))
+print("Country code for Japan")
+print(dic1.get('Japan',"Country no found -"))
