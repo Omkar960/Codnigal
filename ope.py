a32e6f91b5340923fbaa228fe9cc8848ed95f1a0
Omkar960 <wannaagg@gmail.com>
2025-10-21 17:22:53 +0100

    initial commit

---
+++ ope.py
@@ -0,0 +1,12 @@
+set1 = {1,2,3}
+print(set1)
+set2 = {1,2,3,"hello"}
+print(set2)
+set3 = set([1,2,3,4,5])
+print(set3)
+set4 = {1,2,2,3,4,5,5}
+print(set4)
+set5 = {1,2,3,4,5,6}
+print(set5)
+set5.pop()
+print(set5)
