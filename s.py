8efc8d6591ccb5eb152951f20f236436395852c7
Omkar960 <wannaagg@gmail.com>
2025-10-23 19:42:19 +0100

    initial commit

---
+++ s.py
@@ -0,0 +1,11 @@
+
+set1 = {"blue","yellow"}
+set2 = {"green","yellow"}
+set1.symmetric_difference(set2)
+set2.symmetric_difference(set1)
+set1 = {1,2,3,5}
+set2 = {2,4,5,6,}
+set1.symmetric_difference(set2)
+set2.symmetric_difference(set1)
+
+
