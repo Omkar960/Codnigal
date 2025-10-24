400a6ad8bbecd7b3679b67a81143b986d9d1cf39
Omkar960 <wannaagg@gmail.com>
2025-10-24 20:24:46 +0100

    initial commit

---
+++ lcprac.py
@@ -0,0 +1,8 @@
+list = []
+a = int(input("Enter a number:"))
+b =[x for x in range(a+1) if x%2 != 0]
+print(b)
+fruits = ["apple","bannana","pear"]
+fruits =[f.capitalize() for f in fruits]
+print(fruits)
+
