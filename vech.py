cd7f702c91194eb58b0e4c4937fd55ec63d0348c
Omkar960 <wannaagg@gmail.com>
2025-10-31 15:52:32 +0000

    initial commit

---
+++ vech.py
@@ -0,0 +1,10 @@
+class vechile:
+    def __init__(self,maxspeed,mileage):
+        self.maxspeed = maxspeed
+        self.mileage = mileage
+
+car = vechile(240,18)
+
+print(car.maxspeed)
+print(car.mileage)
+        
