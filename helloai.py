b8ebaf5aa117af3f073d659fba2a066cca7ab417
Omkar960 <wannaagg@gmail.com>
2025-11-25 17:07:56 +0000

    commit

--- helloai.py
+++ helloai.py
@@ -10,5 +10,18 @@ elif mood == "bad":
     print("That's not nice to hear")
 else:
     print("It's diffciult to put feelings in words")
-    
+
 print("It was good talking to you!")
+
+sport = input("What sport do you like more, football, basketball or rugby: ")
+
+fav = sport.lower()
+
+if fav == "football":
+    print("That's the most popular sport")
+elif fav == "basketball":
+    print("It originated form Canada")
+elif fav == "rugby":
+    print("It's a very agresssive sport")
+else:
+    print("Couldn't understand your message")
