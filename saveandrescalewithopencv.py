83ea2fa00c7e08331175555b6363bad22ffce318
Omkar960 <wannaagg@gmail.com>
2025-12-16 17:17:16 +0000

    Add files via upload

---
+++ saveandrescalewithopencv.py
@@ -0,0 +1,19 @@
+import cv2
+image = cv2.imread('example.jpg')
+
+gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+resized_image = cv2.resize(gray_image,(800,500))
+
+cv2.imshow('Processed Image',resized_image)
+
+key = cv2.waitKey(0)
+
+if key == ord('s'):
+    cv2.imwrite('grayscae_resized_image.jpg',resized_image)
+    print("Image saved as grayscale_resized_image.jpg")
+else:
+    print("Image not saved")
+cv2.destroyAllWindows()
+
+print(f"Processed Image Dimension:{resized_image.shape}")
