fdcdb60004b1efbe291168b730e88420dce0a5bc
Omkar960 <wannaagg@gmail.com>
2026-01-07 17:26:54 +0000

    Add files via upload

--- facedetectionwithopencv.py
+++ facedetectionwithopencv.py
@@ -1,4 +1,5 @@
 import cv2
+from deepface import DeepFace
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -22,10 +23,33 @@ while True:
     faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors = 5,minSize=(30,30))
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+y,w+h),(255,0,0),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-    cv2.imshow("Face Detection - Press q to Quit",frame)
+        try:
+            face_roi = frame[y:y +h, x:x + w]
 
+            if face_roi.size > 0:
+
+
+                results = DeepFace.analyze(face_roi,actions=["emotion"],enforce_detection=False,silent=True)
+
+                if isinstance(results,list):
+
+                    dominant_emotion = results[0]['dominant_emotion']
+                else:
+                    dominant_emotion = results['dominant_emotion']
+                cv2.putText(frame,dominant_emotion,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.9,(0,255,0),2)
+
+        except Exception as e:
+
+            print(f"Analysis error: {e}")
+        
+    cv2.imshow("Face and Emotion analysis - Press q to Quit",frame)
+
+
+    
+
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
