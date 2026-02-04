ef9ebecf77c4265845fff1f77d13ca204da35f72
Omkar960 <wannaagg@gmail.com>
2026-02-04 17:15:11 +0000

    Add files via upload

---
+++ APItrivia.py
@@ -0,0 +1,54 @@
+import random
+import html
+import requests
+
+EDUCATION_CATEGORY_ID = 9
+api_url = f"https://opentdb.com/api.php?amount=10&category={EDUCATION_CATEGORY_ID}&type=multiple"
+
+def get_question():
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        if data['response_code'] == 0 and data['results']:
+            return data["results"]
+    return None
+def run_quiz():
+    questions = get_question()
+    if not questions:
+
+        print("Failed to fetch educational questions")
+        return
+    
+    score = 0
+    print("Welcome to the Education Quiz")
+
+    for i, q in enumerate(questions,1):
+            questions = html.unescape(q["question"])
+            correct = html.unescape(q["correct_answer"])
+            incorrect = [html.unescape(a)for a in q["incorrect_answers"]]
+
+            options = incorrect + [correct]
+            random.shuffle(options)
+
+            print(f"Questions {i}: {questions} ")
+            for idx, option in enumerate(options,1):
+                print(f"{idx}.{option}")
+            while True:
+                try:
+                    choice = int(input("\n Your answer (1-4)"))
+                    if 1<= choice <= 4:
+                        break
+                except ValueError:
+                    pass
+                print("Invalid input! Please enter 1-4")
+
+            if options[choice-1] == correct:
+                print("Correct \n")
+                score += 1
+            else:
+
+                print(f"Wrong! The correct answer is: {correct}")
+    print(f"Final score is: {score} out of {questions}")
+    print(f"Percentage is: {score/len(questions)*100:.1f}%")
+if __name__ == "__main__":
+    run_quiz()
