dfdfd5e019ad24aaa0afe86c93656e40ac28f1e0
Omkar960 <wannaagg@gmail.com>
2026-02-04 21:01:39 +0000

    Add files via upload

--- APItrivia.py
+++ APItrivia.py
@@ -2,7 +2,7 @@ import random
 import html
 import requests
 
-EDUCATION_CATEGORY_ID = 9
+EDUCATION_CATEGORY_ID = 15
 api_url = f"https://opentdb.com/api.php?amount=10&category={EDUCATION_CATEGORY_ID}&type=multiple"
 
 def get_question():
@@ -21,7 +21,54 @@ def run_quiz():
     
     score = 0
     print("Welcome to the Education Quiz")
-
+    if EDUCATION_CATEGORY_ID == 9:
+        print("General knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 10:
+        print("Book knowledge edition")
+    elif EDUCATION_CATEGORY_ID == 11:
+        print("Movie knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 12:
+        print("Music knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 13:
+        print("Theatre knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 14:
+        print("Youtube knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 15:
+        print("Gaming knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 16:
+        print("Card set knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 17:
+        print("Science knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 18:
+        print("Computer knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 19:
+        print("Maths knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 20:
+        print("Mythology knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 21:
+        print("Sports knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 22:
+        print("Geography knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 23:
+        print("History knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 24:
+        print("Politics knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 25:
+        print("Art knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 26:
+        print("TV knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 27:
+        print("Animal knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 28:
+        print("Train knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 29:
+        print("Superhero knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 30:
+        print("Technology knowledge edition \n")
+    elif EDUCATION_CATEGORY_ID == 31:
+        print("Anime knowledge edition \n")
+    else:
+        print("Disney knowledge edition \n")
     for i, q in enumerate(questions,1):
             questions = html.unescape(q["question"])
             correct = html.unescape(q["correct_answer"])
